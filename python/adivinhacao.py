aee1473a71aadeb91b237449a64762944f6d99d5
hayssakroin <161839031+hayssakroin@users.noreply.github.com>
2025-02-28 08:26:05 -0300

    Add files via upload

---
+++ python/adivinhacao.py
@@ -0,0 +1,18 @@
+import random
+sorteio = random.randint(1,10)
+print(sorteio)
+print("### JOGO DA ADIVINHAÇÃO ###")
+print("Adivinhe o número que estou pensando, de 1 a 10")
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("Tentativa", tentativa)
+    chute = int(input("Digite o seu chute:"))
+    if (chute == sorteio):
+        print("Parabéns, você acertou!")
+        break
+    elif (chute > sorteio):
+        print("Chute um número menor!")
+    elif (chute < sorteio):
+        print("Chute um número maior!")
+    tentativa = tentativa + 1
